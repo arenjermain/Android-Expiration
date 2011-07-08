5dadf301516f8104bf56682a688c1c48b861d686
Sarah Cathey <sscathey@gmail.com>
2011-07-08 12:10:12 -0700

    Implemented to connect our application with zxing barcode scanner. Second button is stub.

---
+++ MoldHold/src/osse/android/moldhold/mainActivity.java
@@ -0,0 +1,69 @@
+package osse.android.moldhold;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+// Application displays two buttons, scan and update (currently a stub). 
+// Clicking the "scan" button invokes the zing application. 
+public class mainActivity extends Activity implements OnClickListener {
+	Button		btnScan;
+	Button		btnUpdate;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		// connect buttons to xml file and set listeners
+		btnScan = (Button) findViewById(R.id.btnScan);
+		btnUpdate = (Button) findViewById(R.id.btnUpdate);
+		btnScan.setOnClickListener(this);
+		btnUpdate.setOnClickListener(this);
+	}
+	
+	
+	
+	// Single onClick handler, uses switch statement to determine which
+	// button was clicked. 
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+			case R.id.btnScan:	// invoke zing scanner application
+				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+				intent.setPackage("com.google.zxing.client.android");
+				intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+				startActivityForResult(intent, 0);
+				break;
+			case R.id.btnUpdate:
+				// STUB
+				break;
+		}
+    }
+
+	
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		// call super??
+	    if (requestCode == 0) {
+	        if (resultCode == RESULT_OK) {
+	            String contents = intent.getStringExtra("SCAN_RESULT");
+	            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+	            // Handle successful scan
+	            // "contents" contains the barcode number
+	            
+	            
+	            
+	            // Will do...
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // Handle cancel
+	        }
+	    } 
+	}
+}
