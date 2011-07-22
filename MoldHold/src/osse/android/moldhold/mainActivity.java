3ea1c82140c5d8d869edd4e39f02003033522586
Sarah Cathey <sscathey@gmail.com>
2011-07-22 12:59:55 -0700

    Started integrating calendar functionality based on calendar-v2-atom-android-sample project by Yaniv Inbar. Added new activity classes (INCOMPLETE). Does not run.

--- MoldHold/src/osse/android/moldhold/mainActivity.java
+++ MoldHold/src/osse/android/moldhold/mainActivity.java
@@ -8,16 +8,27 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 
+
 // Application displays two buttons, scan and update (currently a stub). 
-// Clicking the "scan" button invokes the zing application. 
+// Clicking the "scan" button invokes the zxing application. 
 public class mainActivity extends Activity implements OnClickListener {
-	Button		btnScan;
-	Button		btnUpdate;
+	private static final String 	TAG = "MoldHold";
+	private static final int		ZXING = 0;
+
+	private Button					btnScan;
+	private Button					btnUpdate;
 	
+
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Verify login info is stored in preferences (if not add) and
+		// check that calendar exists (if not create)
+		Intent intent = new Intent(this, calendarSetupActivity.class);
+		startActivity(intent);
+		
 		setContentView(R.layout.main);
 		
 		// connect buttons to xml file and set listeners
@@ -25,8 +36,9 @@ public class mainActivity extends Activity implements OnClickListener {
 		btnUpdate = (Button) findViewById(R.id.btnUpdate);
 		btnScan.setOnClickListener(this);
 		btnUpdate.setOnClickListener(this);
+		
 	}
-	
+
 	
 	
 	// Single onClick handler, uses switch statement to determine which
@@ -38,7 +50,7 @@ public class mainActivity extends Activity implements OnClickListener {
 				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
 				intent.setPackage("com.google.zxing.client.android");
 				intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
-				startActivityForResult(intent, 0);
+				startActivityForResult(intent, ZXING);
 				break;
 			case R.id.btnUpdate:
 				// STUB
@@ -46,24 +58,27 @@ public class mainActivity extends Activity implements OnClickListener {
 		}
     }
 
-	
+
 	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		// call super??
-	    if (requestCode == 0) {
+	    if (requestCode == ZXING) {
 	        if (resultCode == RESULT_OK) {
 	            String contents = intent.getStringExtra("SCAN_RESULT");
 	            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
 	            // Handle successful scan
 	            // "contents" contains the barcode number
 	            
-	            
-	            
-	            // Will do...
+	            // Intent intent = new Intent(this, <db??>.class);
+	            // startActivity(intent);
+	            // activate database activity
 	        } else if (resultCode == RESULT_CANCELED) {
 	            // Handle cancel
 	        }
 	    } 
 	}
+	
+	
+	
 }
