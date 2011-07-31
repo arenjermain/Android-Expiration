7751255986cdbd91aff7421b545042d054aa26fe
michelle <mi.l.carter@gmail.com>
2011-07-31 14:05:51 -0700

    Changed mainActivity to match Sarah's with minor punctuation changes.

--- MoldHold/src/osse/android/moldhold/mainActivity.java
+++ MoldHold/src/osse/android/moldhold/mainActivity.java
@@ -7,21 +7,43 @@ package osse.android.moldhold;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 
+
 // Application displays two buttons, scan and update (currently a stub). 
 // Clicking the "scan" button invokes the zxing application. 
+// Clicking the "scan" button invokes the zxing application. 
+//
+// This application uses SharedPreferences which contains the following keys:
+//		"AUTH_TOKEN"
+//		"GSESSION_ID"
+//		"ACCOUNT_NAME"
+//
 public class mainActivity extends Activity implements OnClickListener {
-	Button		btnScan;
-	Button		btnUpdate;
+	private static final String 	TAG = "MoldHold";
+	private static final int		REQUEST_SETUP = 0;
+	private static final int		REQUEST_ZXING = 1;
+
+	private Button					btnScan;
+	private Button					btnUpdate;
+	private Button					btnQuit;
 	
+
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Verify login info is stored in preferences (if not add) and
+		// check that calendar exists (if not create)
+		Log.d(TAG, "initiating intent for calendarSetupActivity...");
+		Intent intent = new Intent(this, calendarSetupActivity.class);
+		startActivityForResult(intent, REQUEST_SETUP);
+		/*
 		setContentView(R.layout.main);
 		
 		// connect buttons to xml file and set listeners
@@ -29,7 +51,9 @@ public class mainActivity extends Activity implements OnClickListener {
 		btnUpdate = (Button) findViewById(R.id.btnUpdate);
 		btnScan.setOnClickListener(this);
 		btnUpdate.setOnClickListener(this);
+		*/
 	}
+	
 	
 	
 	
@@ -42,32 +66,51 @@ public class mainActivity extends Activity implements OnClickListener {
 				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
 				intent.setPackage("com.google.zxing.client.android");
 				intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
-				startActivityForResult(intent, 0);
+				startActivityForResult(intent, REQUEST_ZXING);
 				break;
 			case R.id.btnUpdate:
 				// STUB
 				break;
+			case R.id.btnQuit:
+				finish();
 		}
     }
 
-	
+
 	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		// call super??
-	    if (requestCode == 0) {
+	    if (requestCode == REQUEST_ZXING) {
 	        if (resultCode == RESULT_OK) {
 	            String contents = intent.getStringExtra("SCAN_RESULT");
 	            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
 	            // Handle successful scan
 	            // "contents" contains the barcode number
 	            
-	            
-	            
-	            // Will do...
+	            // Intent intent = new Intent(this, <db??>.class);
+	            // startActivity(intent);
+	            // activate database activity
 	        } else if (resultCode == RESULT_CANCELED) {
-	            // Handle cancel
+	            // Handle cancel...
 	        }
-	    } 
+	    } else if (requestCode == REQUEST_SETUP) {
+	    	if (resultCode == RESULT_OK) {
+	    		// no extras to retreive...??
+	    		setContentView(R.layout.main);
+	    		
+	    		// connect buttons to xml file and set listeners
+	    		btnScan = (Button) findViewById(R.id.btnScan);
+	    		btnUpdate = (Button) findViewById(R.id.btnUpdate);
+	    		btnQuit = (Button) findViewById(R.id.btnQuit);
+	    		
+	    		btnQuit.setOnClickListener(this);
+	    		btnScan.setOnClickListener(this);
+	    		btnUpdate.setOnClickListener(this);
+	    	}
+	    }
 	}
+	
+	
+	
 }
