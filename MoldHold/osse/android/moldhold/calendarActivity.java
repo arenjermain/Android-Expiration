f20eb37fa5ef039402d574c8009fa1d87dc72267
aren <aren@pdx.edu>
2011-08-07 14:04:39 -0700

    stoopid commit to switch branch
    Change-Id: I48d0cc5dbbc51f2d0212a3a94482aaef26d5946c

--- MoldHold/osse/android/moldhold/calendarActivity.java
+++ MoldHold/osse/android/moldhold/calendarActivity.java
@@ -1,6 +1,6 @@
 
 /*
-Copyright © 2011 Sarah Cathey, Michelle Carter, Aren Edlund-Jermain
+Copyright ï¿½ 2011 Sarah Cathey, Michelle Carter, Aren Edlund-Jermain
 This project is protected under the Apache license. 
 Please see COPYING file in the distribution for license terms.
 */
